b0f2ffc26f0dae7ce8218b6bbd70e38b343d2bab
hu <2546610233@qq.com>
2025-04-16 12:33:22 +0800

    增加数据

--- resources/book_resource.py
+++ resources/book_resource.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
+from flask import request
 from flask_restful import Resource
 
+from models.book_model import BookModel
 from resources import api
 from services.book_service import BookService
 
@@ -16,4 +20,25 @@ class BookResource(Resource):
         return {'id': student_id, 'name': 'Mary', 'gender': 'famale'}
 
 
+class BookListResource(Resource):
+    def get(self):
+        book_list = BookService().get_all_books()
+        return [book_model.serialize() for book_model in book_list]
+
+    def post(self):
+        request_json = request.json
+        if request_json:
+            name = request_json.get('name')
+            author = request_json.get('author')
+            publish_time = datetime.fromisoformat(request_json.get('publish_time', None))
+
+            book_model = BookModel(name=name, author=author, publish_time=publish_time)
+            BookService().create_book(book_model)
+
+            return book_model.serialize()
+        else:
+            return {'error': 'Please provide Book info as a Json'}, 400
+
+
 api.add_resource(BookResource, '/books/<int:book_id>')
+api.add_resource(BookListResource, '/books')
--- services/book_service.py
+++ services/book_service.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Select, asc
+
 from models.book_model import BookModel
 from resources import db
 
@@ -5,3 +7,26 @@ from resources import db
 class BookService:
     def get_book_by_id(self, book_id: int):
         return db.session.get(BookModel, book_id)
+
+    def get_all_books(self):
+        query = Select(BookModel).order_by(asc(BookModel.name))
+        return db.session.scalars(query).all()
+
+    def create_book(self, book_model: BookModel):
+        db.session.add(book_model)
+        db.session.commit()
+
+        return book_model
+
+    def update_book(self, book_id: int, book: BookModel):
+        book_to_update = db.session.get(BookModel, book_id)
+        book_to_update.name = book.name
+        book_to_update.author = book.author
+        book_to_update.year = book.year
+        db.session.commit()
+        return book_to_update
+
+    def delete_book(self, book_id: int):
+        book_to_delete = db.session.get(BookModel, book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
